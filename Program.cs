b908acdc5b12a7d2490fca0f21fae76fc46fb6dd
RaymondL <raymondlaghaeian@users.noreply.github.com>
2016-04-15 12:52:46 -0700

    Update Program.cs
    Added list end points function

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
@@ -11,12 +11,13 @@ namespace AddEndpoint
     {
         static void Main(string[] args)
         {
-            const string endpointName = "newendpoint1";//Set name
-            const string workspaceId = "c79283d685684d8488fc...";//Get from workspace settings
-            const string workspaceToken = "7df5facee554413399...";//Get from Settings, Auth Token
-            const string webserviceId = "123e37e77a534a038e...";//Get from Azure portal or help page url of the service after /webservices/
+            const string endpointName = "newendpoint1";
+            const string workspaceId = "000bf7ab905345088dfa1843d6";//Replace with your id from workspace settings
+            const string workspaceToken = "6e8a7d5a5e0a4c0e2a9a761bd48";//Replace with your id from Settings, Auth Token
+            const string webserviceId = "b68ff4d7cfde431c5efcc1c3f9";//Replace with your id from Azure portal or help page url of the service after /webservices/
 
             string endpointUrl = getEndpointUrl(workspaceId, webserviceId, endpointName);
+            string endpointsUrl = getEndpointsUrl(workspaceId, webserviceId);//to get endpoints list
 
             //Delete endpoint
             Console.WriteLine("Delete endpoint? y/n");
@@ -32,6 +33,15 @@ namespace AddEndpoint
             if (responseCreate == "y")
             {
                 AddNewEndpoint(endpointUrl, workspaceToken).Wait();
+            }
+
+            //Get endpoints
+            //Add new endpoint
+            Console.WriteLine("Get endpoints? y/n");
+            string responseGet = Console.ReadLine();
+            if (responseGet == "y")
+            {
+                ListEndpoints(endpointsUrl, workspaceToken).Wait();
             }
             Console.WriteLine("Press enter to close.");
             Console.ReadLine();
@@ -56,7 +66,7 @@ namespace AddEndpoint
                     Content = new StringContent(JsonConvert.SerializeObject(request), Encoding.Default, "application/json")
                 };
 
-                Console.WriteLine("Starting create endpoint.");
+                Console.WriteLine("Starting create endpoint");
 
                 var response = await client.SendAsync(requestMessage);
                 string responseContent = await response.Content.ReadAsStringAsync();
@@ -79,10 +89,18 @@ namespace AddEndpoint
         
         static string getEndpointUrl(string workspaceId, string webserviceId, string endpointName)
         {
+
             //Format is: https://management.azureml.net/workspaces/<workspaceId>/webservices/<webserviceid>/endpoints/<name> 
             //You can get the workspaceId and webesrviceId from Scoring service's RRS help page url (remove apihelp and id after endpoints)
             string endpointUrl = "https://management.azureml.net/workspaces/" + workspaceId + "/webservices/" + webserviceId + "/endpoints/" + endpointName;
             return endpointUrl;
+        }
+        static string getEndpointsUrl(string workspaceId, string webserviceId)
+        {
+            //Format is: https://management.azureml.net/workspaces/<workspaceId>/webservices/<webserviceid>/endpoints
+            //You can get the workspaceId and webesrviceId from Scoring service's RRS help page url (remove apihelp and id after endpoints)
+            string endpointsUrl = "https://management.azureml.net/workspaces/" + workspaceId + "/webservices/" + webserviceId + "/endpoints";
+            return endpointsUrl;
         }
         /********************************Delete endpoint*****************************/
         static async Task DeleteEndpoint(string requestUri, string apiKey)
@@ -107,6 +125,31 @@ namespace AddEndpoint
                 {
                     Console.WriteLine("Response status code {0}", response.StatusCode);
                     Console.WriteLine("Endpoint deletion failed: {0}", response.ToString());
+                }
+            }
+        }
+        /**********************List endpoints*************************************************/
+        static async Task ListEndpoints(string requestUri, string apiKey)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+                var requestMessage = new HttpRequestMessage(new HttpMethod("GET"), requestUri) { };
+
+                Console.WriteLine("Starting getting list of endpoints.");
+
+                var response = await client.SendAsync(requestMessage);
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                if (response.IsSuccessStatusCode)
+                {
+
+                    Console.WriteLine(responseContent.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Response status code {0}", response.StatusCode);
+                    Console.WriteLine("ListEndpoints failed: {0}", response.ToString());
                 }
             }
         }
